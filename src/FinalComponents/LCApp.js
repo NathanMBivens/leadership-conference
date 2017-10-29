c98c9e859fdf713363b3d2df6294678a0484d928
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-29 15:23:45 -0500

    React-router implementation. May not need it.

--- src/FinalComponents/LCApp.js
+++ src/FinalComponents/LCApp.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Navigation from './Navigation_LC';
 
 export default class LCApp extends React.Component {
   render() {
     return (
-      <div>
-        Hello.
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Navigation} />
+        </div>
+      </Router>
     )
   }
 }
