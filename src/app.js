de7c2d8200db9e77e77e9eb3b8d5bce121b08026
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-29 15:24:51 -0500

    Added styles to app component.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LCApp from './FinalComponents/LCApp';
+import style from './styles/style.scss';
 
 ReactDOM.render(<LCApp />, document.getElementById('app'));
