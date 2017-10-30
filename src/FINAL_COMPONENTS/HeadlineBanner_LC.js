5203bb6a7dca04f75497de21951c266797788d19
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-29 20:48:37 -0500

    The immediate banner component that will be sent to LCApp.js

---
+++ src/FINAL_COMPONENTS/HeadlineBanner_LC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeadlineBanner from '../Components/HeadlineBanner/_Banner';
+
+export default class HeadlineBanner_LC extends React.Component {
+  render() {
+    return (
+      <div>
+        <HeadlineBanner />
+      </div>
+    )
+  }
+}
