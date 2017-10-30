35ad4fa6b5716c21b165e8af1a8f5f9412fa0981
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-29 20:49:41 -0500

    Navigation component sent to LCApp.js

---
+++ src/FINAL_COMPONENTS/Navigation_LC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Components/Navigation/_Navigation';
+
+export default class Navigation_LC extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+      </div>
+    )
+  }
+}
