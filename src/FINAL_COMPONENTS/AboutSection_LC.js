5ec75f6574f8ee1f3b693b1867b28db2146eab35
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-31 08:33:55 -0500

    Holds the About Section final component.

---
+++ src/FINAL_COMPONENTS/AboutSection_LC.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutSection from '../Components/AboutSection/_AboutSection';
+
+export default class HeadlineBanner_LC extends React.Component {
+  render() {
+    return (
+      <div>
+        <AboutSection />
+      </div>
+    )
+  }
+}
