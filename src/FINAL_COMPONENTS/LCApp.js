ef38ae7f5a10aae890cc042c1596d52acaae4ca1
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-30 19:37:25 -0500

    Updated LCApp to include each sectionthus far.

--- src/FINAL_COMPONENTS/LCApp.js
+++ src/FINAL_COMPONENTS/LCApp.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Navigation_LC from './Navigation_LC';
 import HeadlineBanner_LC from './HeadlineBanner_LC';
+import AboutSection_LC from './AboutSection_LC';
+import SpeakersSection_LC from './SpeakersSection_LC';
+import ScheduleSection_LC from './ScheduleSection_LC';
+
 
 export default class LCApp extends React.Component {
   render() {
@@ -10,6 +14,9 @@ export default class LCApp extends React.Component {
         <div>
           <Navigation_LC />
           <HeadlineBanner_LC />
+          <AboutSection_LC />
+          <SpeakersSection_LC />
+          <ScheduleSection_LC />
         </div>
 
     )
