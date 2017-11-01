407c80e0c718d7db712697b143045d305b9dec2f
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-31 21:58:52 -0500

    About section. Pretty simple component.

---
+++ src/Components/AboutSection/_AboutSection.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class AboutSection extends React.Component {
+  render() {
+    return (
+      <div className="about-section">
+        <div className="section-container">
+          <div className="about-section-title"> About Leadership Conference
+          </div>
+          <div className="about-section-text">
+          Leadership Conference has always been about
+          reaching out to business leaders, churches,
+          startups, CEOs, and industry leaders to help
+          them understand, equip, and grow themselves
+          and their organizations to reach the world
+          around them.
+          </div>
+          <div className="about-section-text">
+          For the past 10 years, we have seen and heard
+          from thousands of our attendees their successes
+          as a result of their time at Leadership Conference.
+          </div>
+          <div className="about-section-text">
+          We don’t take it lightly. That’s why we have
+          dedicated ourselves to bringing in the top leaders
+          in their respective fields. We don’t ask for one-hour
+          motivational pep talks. We require a comprehensive,
+          tried-and-true plan to implement success.
+          </div>
+          <div className="about-section-text">
+          These top leaders have seen and worked for their
+          successes. Now it’s your turn.
+          </div>
+          <div className="about-section-text">
+          Are you ready?
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
