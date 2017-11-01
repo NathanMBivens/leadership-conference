b4201fa35a6938633b66fac808747abbeb80d3a8
Nathan Bivens <bivensfamily@nathans-imac.attlocal.net>
2017-10-31 21:56:50 -0500

    Navigation component to send to the final version of this component.

---
+++ src/Components/Navigation/_Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LCButton from './LCButton';
+import NavigationBar from './NavigationBar';
+
+export default class Navigation extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavigationBar />
+
+      </div>
+    )
+  }
+}
